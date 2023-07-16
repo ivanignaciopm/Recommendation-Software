75982fe3744e1f39e206cff888a97c6ba2b2f843
lacrimxsa7 <ivanignaciopm@gmail.com>
2023-07-15 22:39:30 -0400

    data gathering

---
+++ data.py
@@ -0,0 +1,12 @@
+data = [
+['Attraction', 'Cerro San Cristóbal', '4', '4', 'Pío Nono 450, Recoleta, Santiago'],
+['Attraction', 'Plaza de Armas', '5', '5', 'Santiago, Metropolitan Region'],
+['Attraction', 'Barrio Lastarria', '4', '4', 'José Victorino Lastarria, Santiago, Metropolitan Region'],
+['Attraction', 'Mercado Central', '4', '4', 'San Pablo 967, Santiago, Metropolitan Region'],
+['Attraction', 'Museo de Bellas Artes', '4', '4', 'Parque Forestal s/n, Santiago, Metropolitan Region'],
+['Restaurant', 'German Cuisine - Boragó', '5', '5', 'Nueva Costanera 3467, Vitacura'],
+['Restaurant', 'Contemporary Chilean Cuisine - Como Agua para Chocolate', '4', '4', 'Constitución 88, Providencia'],
+['Restaurant', 'Peruvian-Chilean Cuisine - Astrid y Gastón', '4', '4', 'Antonio Bellet 201, Providencia'],
+['Restaurant', 'Chilean Cuisine - Liguria', '4', '4', 'Loreto 110, Recoleta'],
+['Restaurant', 'Ancestral Chilean Cuisine - Peumayén Ancestral Food', '4', '4', 'Constitución 136, Providencia']
+]
